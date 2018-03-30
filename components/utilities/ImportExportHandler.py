345ebf6544118e9356cfaed62b68b5e661b786be
Edcel Celiz <edceliz01@gmail.com>
2018-03-30 19:42:36 +0800

    Provided save/load and import/export features.

---
+++ components/utilities/ImportExportHandler.py
@@ -0,0 +1,45 @@
+from PyQt5 import QtWidgets
+import csv
+from components import Database as db
+
+def getCSVFile(type):
+    fileName = QtWidgets.QFileDialog().getOpenFileName(None, 'Import CSV File', '', 'CSV File (*.csv)')
+    if not fileName[0]:
+        return False
+    file = open(fileName[0], 'r')
+    with file:
+        fileContent = csv.reader(file)
+        content = []
+        for index, row in enumerate(fileContent):
+            if not index and row[0] != type:
+                return False
+            content.append(row)
+    if not fileContent.line_num:
+        return False
+    return content
+
+def saveAs():
+    fileName = QtWidgets.QFileDialog.getSaveFileName(None,  'Save GAS Scenario', '', 'GAS Scenario (*.gas)')
+    with open(fileName[0], 'w+') as file:
+        conn = db.getConnection()
+        for line in conn.iterdump():
+            file.write('{}\n'.format(line))
+        conn.close()
+
+def load():
+    fileName = QtWidgets.QFileDialog().getOpenFileName(None, 'Load GAS Scenario', '', 'GAS Scenario (*.gas)')
+    with open(fileName[0], 'r') as file:
+        conn = db.getConnection()
+        cursor = conn.cursor()
+        tables = list(cursor.execute("SELECT name FROM sqlite_master WHERE type IS 'table'"))
+        cursor.executescript(';'.join(['DROP TABLE IF EXISTS {}'.format(table[0]) for table in tables]))
+        cursor.executescript(file.read())
+        conn.close()
+
+# con = sqlite3.connect('../sqlite.db')
+# with open('../dump.sql', 'w') as f:
+#     for line in con.iterdump():
+#         f.write('%s\n' % line)
+#
+# sql = f.read() # watch out for built-in `str`
+# cur.executescript(sql)
